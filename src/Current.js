fda3c0c01e1b983a1cfbe816f9072f180f27bf00
Egleta <eglaite.t@gmail.com>
2021-10-31 21:52:37 +0100

    current weather data fixed

--- src/Current.js
+++ src/Current.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function Current() {
+export default function Current(props) {
     return (
         <div className="Current">
             <h5 className="card-subtitle mb-0 mt-2">
                 Currently in
-                <strong id="city">–</strong>:
+                <strong id="city"> {props.weather.city}</strong>:
             </h5>
             <div className="row mb-5">
                 <div className="col me-3">
                     <div className="row align-items-center">
                         <div className="col-4 p-0">
-                            <img alt="Weather conditions" id="bigIcon" />
+                            <img alt="Weather conditions" id="bigIcon" src={props.weather.icon} />
                         </div>
                         <div className="col-6 ps-0">
-                            <span id="currentTemperatureBig">–</span>
+                            <span id="currentTemperatureBig">{Math.round(props.weather.temperature)}</span>
                             <span id="CF">
                                 <button id="currentC" className="selected">
                                     °C
@@ -25,14 +25,11 @@ export default function Current() {
                     </div>
                     <div className="mainWeatherInfo">
                         <div>
-                            <span id="weatherConditionsCurrently"></span>,<span id="dayOrNight"></span>
+                            <span id="weatherConditionsCurrently">{props.weather.description}</span>,{" "}
+                            {props.weather.dayOrNight}
                         </div>
-                        <div>
-                            Humidity: <span id="humidityCurrently">–</span> %
-                        </div>
-                        <div>
-                            Wind: <span id="windCurrently">–</span> m/s
-                        </div>
+                        <div>Humidity: {props.weather.humidity} %</div>
+                        <div>Wind: {Math.round(props.weather.wind)} m/s</div>
                     </div>
                 </div>
             </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import SearchForm from "./SearchForm";
 import Current from "./Current";
 import Globe from "./Globe";
 import Forecast from "./Forecast";
 
 export default function Weather() {
     const apiKey = "a85784d2dae7d5a007ca536ecd5baadb";
-    let city = "London";
-    let apiURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let [currentWeatherData, setCurrentWeatherData] = useState("");
+    //let [forecast, setForecast] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const city = event.target.city.value;
+        const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiURL).then(handleResponse);
+    }
+
+    function handleResponse(response) {
+        setCurrentWeatherData({
+            city: response.data.name,
+            temperature: response.data.main.temp,
+            wind: response.data.wind.speed,
+            humidity: response.data.main.humidity,
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            description: response.data.weather[0].description,
+            dayOrNight: stateOfTheDay(response.data.weather[0].icon),
+        });
+
+        function stateOfTheDay(iconName) {
+            if (iconName.slice(iconName.length - 1) === "n") {
+                return "Night";
+            }
+            return "Day";
+        }
+    }
+
     return (
         <div className="Weather">
             <div className="row">
                 <div className="col">
-                    <SearchForm />
-                    <Current />
+                    <div className="SearchForm">
+                        <form onSubmit={handleSubmit}>
+                            <div className="row">
+                                <div className="col-9">
+                                    <div className="mb-3">
+                                        <input
+                                            type="search"
+                                            name="city"
+                                            className="form-control"
+                                            placeholder="Enter your city"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-2">
+                                    <button type="submit" className="btn btn-secondary">
+                                        Search
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <Current weather={currentWeatherData} />
                 </div>
                 <div className="col pt-4">
                     <Globe />
